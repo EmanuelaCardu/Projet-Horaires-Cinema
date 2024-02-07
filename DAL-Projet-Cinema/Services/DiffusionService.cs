d03fd19e4da204516a90dd19abc7a5c4d1a41bcd
EmanuelaCardu <emanuelacardu@gmail.com>
2024-02-07 12:51:03 +0100

    DiffusionServiceDAL

--- DAL-Projet-Cinema/Services/DiffusionService.cs
+++ DAL-Projet-Cinema/Services/DiffusionService.cs
@@ -18,7 +18,18 @@ namespace DAL_Projet_Cinema.Services
 
         public void Delete(int id)
         {
-           
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_Diffusion_Delete";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id_Diffusion", id);
+                    connection.Open();
+                    if (command.ExecuteNonQuery() <= 0)
+                        throw new ArgumentException(nameof(id), $"L'identifiant {id} n'est pas das la base de données");
+                }
+            }
         }
 
         public IEnumerable<Diffusion> Get()
@@ -43,17 +54,88 @@ namespace DAL_Projet_Cinema.Services
 
         public Diffusion Get(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_Diffusion_GetById";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id_Movie", id);
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read()) return reader.ToDiffusion();
+                        throw new ArgumentException(nameof(id), $"L'identifiant {id} n'existe pas dans la base de données.");
+                    }
+                }
+            }
+        }
+
+
+        public IEnumerable<Diffusion> GetByCinemaPlace( int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_Diffusion_GetByCinemaPlace";
+                    command.Parameters.AddWithValue("id_cinemaPlace", id);
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return reader.ToDiffusion();
+                        }
+                    }
+                }
+            }
         }
 
         public int Insert(Diffusion data)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_Diffusion_Insert";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("DiffusionDate", data.DiffusionDate);
+                    command.Parameters.AddWithValue("DiffusionTime", data.DiffusionTime);
+                    command.Parameters.AddWithValue("AudioLang", data.AudioLang);
+                    command.Parameters.AddWithValue("SubTitleLang", data.SubTitleLang ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("Id_CinemaRoom", data.Id_CinemaRoom);
+                    command.Parameters.AddWithValue("Id_Movie", data.Id_Movie);
+                    connection.Open();
+                    return (int)command.ExecuteScalar();
+                }
+            }
         }
 
         public void Update(Diffusion data)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_Diffusion_Update";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("DiffusionDate", data.DiffusionDate);
+                    command.Parameters.AddWithValue("DiffusionTime", data.DiffusionTime);
+                    command.Parameters.AddWithValue("AudioLang", data.AudioLang);
+                    command.Parameters.AddWithValue("SubTitleLang", data.SubTitleLang ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("Id_CinemaRoom", data.Id_CinemaRoom);
+                    command.Parameters.AddWithValue("Id_Movie", data.Id_Movie);
+                    connection.Open();
+                    if (command.ExecuteNonQuery() <= 0)
+                        throw new ArgumentException(nameof(data.Id_Movie), $"L'identifiant {data.Id_Diffusion} n'est" +
+                            $" pas dans la base de donnée");
+
+
+                }
+
+            }
         }
     }
 }
